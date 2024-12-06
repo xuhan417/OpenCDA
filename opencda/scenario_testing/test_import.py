8ae38f2e0d18391fd1d95417767411e05bde9455
xuhan <hanxu417@g.ucla.edu>
2024-12-06 12:16:12 -0800

    open scenario integration and srunner integration to opencda root

---
+++ opencda/scenario_testing/test_import.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# License: TDG-Attribution-NonCommercial-NoDistrib
+
+import carla
+import opencda.scenario_testing.utils.sim_api as sim_api
+from opencda.core.common.cav_world import CavWorld
+
+import time
+from multiprocessing import Process
+
+import scenario_runner as sr
+
+print('successful import scenario runner!')
